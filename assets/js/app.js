144ac24438578e8496f33f7c87b3cccd756c4b3c
Nupur Bishnoi <nupurbishnoi03@gmail.com>
2025-07-01 14:52:42 +0530

    accordian section done

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,3 +10,35 @@ const toggleNavbar = () => {
     toggleBar3.classList.toggle('-rotate-[35deg]')
     document.querySelector('html').classList.toggle('overflow-y-hidden')
 }
+
+const items = document.querySelectorAll('.accordion-item');
+
+items.forEach(item => {
+    const header = item.querySelector('.accordion-header');
+    const content = item.querySelector('.accordion-content');
+    const arrow = item.querySelector('.arrow-icon');
+
+    header.addEventListener('click', () => {
+        const isOpen = item.classList.contains('active');
+
+        items.forEach(i => {
+            i.classList.remove('active');
+            i.classList.add('rounded-full');
+            i.style.borderRadius = '50px';
+            i.classList.remove('shadow-md', 'border', 'border-black/10');
+            i.querySelector('.accordion-content').style.maxHeight = null;
+            i.querySelector('.arrow-icon').classList.remove('rotate-90');
+        });
+
+        if (!isOpen) {
+            item.classList.add('active');
+            item.classList.remove('rounded-full');
+            item.style.borderRadius = '30px';
+            item.classList.add('shadow-md', 'border', 'border-black/10');
+            content.style.maxHeight = content.scrollHeight + "px";
+            arrow.classList.add('rotate-90');
+        }
+    });
+});
+
+
